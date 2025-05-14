1d7761f12a8a055699955a5f2684b1b1dfae2bc5
drewcrix <drewcrix@gmail.com>
2025-05-14 18:04:45 -0400

    Update db_credentials.php

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -5,7 +5,7 @@
 // 2. Unique credentials on development and production servers
 // 3. Unique credentials if working with multiple developers
 
-define("DB_SERVER", "sqlsrv:Server=rpqatrack03vm;Database=VCTdata");
+define("DB_SERVER", "sqlsrv:Server= ServerName");
 define("DB_USER", "vctuser");
 define("DB_PASS", "databasePassword");
 
